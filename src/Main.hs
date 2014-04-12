7f46aba7aa33b0800127040a3da8de600501a239
Nulldata <thebreadcat@gmail.com>
2014-04-12 17:50:33 +0200

    Added replying and text-sanitization

--- src/Main.hs
+++ src/Main.hs
@@ -14,7 +14,6 @@ import           Control.Lens
 --import           Control.Lens.TH
 import           Data.UID (newUIDString)
 import           System.Directory
-import           Data.List.Split (splitOn)
 import qualified Data.ByteString.Char8      as BS
 import           Snap.Snaplet.AcidState
 import           BoardApp
@@ -60,10 +59,13 @@ tryTop = ifTop $ uncachedHandler $ serveFile "res/index.html"
 
 -- Some people just want to watch the world burn.
 routes :: [ (BS.ByteString, HHandler ()) ]
-routes = [ ("res/"  , serveDirectory "res")
-         , ("post"   , method POST performPost)
+routes = [ ("res/", serveDirectory "res")
+         , ("/", serveDirectory "res")
+         , ("post", method POST performPost)
+         , ("reply", method POST performReply)
          , ("getposts/:pidx/:pcnt", withContentType "text/json" fetchPosts)
          , ("getpost/:pid", withContentType "text/json" fetchPost)
+         , ("getpreview/:pid", withContentType "text/json" fetchPreview)
          , ("dragons", dragons) ]
 
 -- Shrek plz.
@@ -81,8 +83,7 @@ hboardAppInit = makeSnaplet "hboard" "An image-board server!" Nothing $ do
      else return ()
   return $ HboardApp b
 
-fetchPost :: HHandler ()
-fetchPost = do
+fetchEntry getter = do
   getParam "pid"
            >>= retPost
            >>= maybe (withResponseCode 404 $ writeBS "{}")
@@ -90,13 +91,31 @@ fetchPost = do
   where
     retPost Nothing  = return Nothing
     retPost (Just x) = do
-      n <- query (GetPost $ BS.unpack x)
+      n <- query (getter $ BS.unpack x)
       return n
+
+fetchPost :: HHandler ()
+fetchPost = fetchEntry GetPost
+
+fetchPreview :: HHandler ()
+fetchPreview = fetchEntry GetPreview
 
 readDecMaybe :: BS.ByteString -> Maybe Int
 readDecMaybe s = case readDec $ BS.unpack s of
   [ (i, "") ] -> Just i
   _           -> Nothing
+
+
+-- Sanitization for text that goes into the body of a html tag
+-- This does not sanitize for attributes
+-- Esacped chars: < > &
+sanitizeText :: BS.ByteString -> BS.ByteString
+sanitizeText "" = ""
+sanitizeText te = BS.foldl (\acc x -> BS.append acc $ maybe (BS.singleton x) id (lookup x escapedChars)) "" te
+  where
+    escapedChars = [ ('<', "&lt;")
+                   , ('>', "&gt;")
+                   , ('&', "&amp;") ];
 
 fetchPosts :: HHandler ()
 fetchPosts = do
@@ -112,9 +131,14 @@ handleImageUpload :: MonadIO m =>
                      [(PartInfo, Either PolicyViolationException FilePath)]
                      -> m (Either BS.ByteString FilePath)
 handleImageUpload [ (pain, Right fp) ] = do
-  nfn <- newImage
-  liftIO $ renameFile fp nfn
-  return $ Right nfn
+  nfn <- newImage $ partContentType pain
+  case nfn of
+    Nothing -> return $ Left $ if partFileName pain == Just ""
+                                  then "No file supplied."
+                                  else "File type either not valid or not supported."
+    Just x  -> do
+      liftIO $ renameFile fp x
+      return $ Right x
 
 handleImageUpload [ (_, Left polex) ] = return $ Left $ BS.pack $ show polex
 
@@ -122,16 +146,35 @@ handleImageUpload [] = return $ Left "Invalid form."
 
 handleImageUpload (_:_) = return $ Left "Multiple files submitted!"
 
-fileExtension :: FilePath -> FilePath
-fileExtension fp = (last $ splitOn "." fp)
-
-newImage :: MonadIO m => m FilePath
-newImage = do
-  nuid <- liftIO newUIDString
-  let nfilename = mkfn nuid
-  return $ nfilename
+fileExtension :: BS.ByteString -> Maybe FilePath
+fileExtension fp = lookup fp allowedMIMEs
   where
-    mkfn x = "res/img/" ++ x
+    allowedMIMEs = [ ( "image/png", ".png" )
+                   , ( "image/jpeg", ".jpeg" )
+                   , ( "image/gif", ".gif" )
+                   , ( "image/webp", ".webp" )
+                   , ( "video/wemb", ".webm" ) ];
+
+newImage :: MonadIO m => BS.ByteString -> m (Maybe FilePath)
+newImage ct = do
+  nuid <- liftIO newUIDString
+  return $ fext >>= Just . (mkfn nuid)
+  where
+    fext = fileExtension ct
+    mkfn x y = "res/img/" ++ x ++ y
+
+validateImage :: HHandler (Either BS.ByteString FilePath)
+validateImage = handleFileUploads "./tmp"
+                                  defaultUploadPolicy
+                                  (const $ allowWithMaximumSize 1572864)
+                                  handleImageUpload
+
+maybeImage :: HHandler (Maybe (Either BS.ByteString FilePath)) 
+maybeImage = do
+  img <- validateImage
+  return $ if img == Left "No file supplied."
+              then Nothing
+              else Just img
 
 performPost :: HHandler ()
 performPost = do
@@ -144,14 +187,27 @@ performPost = do
                                   (formulateRepsonse fp)
                                   x
     formulateRepsonse fp t = do
-      nid <- query NextEntryId
-      update $ PostThread (ThreadPost (show nid) fp t [])
+      update $ PostThread fp (sanitizeText t)
       writeBS $ BS.append (BS.pack $ "Image: " ++ fp ++ "\n") t
 
-    validateImage = handleFileUploads "./tmp"
-                                      defaultUploadPolicy
-                                      (const $ allowWithMaximumSize 1572864)
-                                      handleImageUpload
+performReply :: HHandler ()
+performReply = do
+  -- Required form fields:
+  pimg <- maybeImage
+  getPostParam "to" >>= maybe (writeBS "Invalid form.")
+                              (optionals pimg)
+  where
+    -- Optional form fields, the text field will be substituded 
+    -- with an empty string if not found.
+    optionals pimg post = do
+      text <- getPostParam "text" >>= maybe (return "") (return . id)
+      case pimg of
+        Just (Left err)  -> writeBS err
+        Just (Right img) -> doReply post text (Just img)
+        Nothing          -> doReply post text Nothing
+    doReply post text mimg = do
+      update $ ReplyTo mimg (sanitizeText text) (BS.unpack post)
+      writeBS "Replied!"
 
 
 main :: IO ()
